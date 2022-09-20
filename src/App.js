dfa7cdfbe140eb69600ce5115ea98b4c5f1c0d0d
RickHayden <richard.hayden459@live.com>
2022-09-20 13:13:09 -0400

    api linked

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine 
+    height="100vh"
+    projectID="1f512ebe-c21c-4249-8510-db906252e132"
+    userName="Rick"
+    userSecret="rick"
+    />
+
   );
 }
 
